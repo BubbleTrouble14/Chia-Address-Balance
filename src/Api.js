a59c5af51d0b72cccd3817f9708d4c5c9b6fa38e
BubbleTrouble14 <ronald.goedeke@outlook.com>
2021-07-05 22:25:52 +0200

    Changed api call back to using webserver

--- src/Api.js
+++ src/Api.js
@@ -12,12 +12,37 @@ import React, { useEffect, useState } from 'react';
 //   });
 // };
 
+// export const getBalance = (address) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(JSON.parse('{"grossBalance":11432600000100,"netBalance":11432600000100}'));
+//     }, 500);
+//   })
+//     .then((json) => {
+//       return json;
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// };
+
+// export const getCurrentPrice = () => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(JSON.parse('{"price":277.53}'));
+//     }, 500);
+//   })
+//     .then((json) => {
+//       return json;
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// };
+
 export const getBalance = (address) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(JSON.parse('{"grossBalance":11432600000100,"netBalance":11432600000100}'));
-    }, 500);
-  })
+  return fetch('https://api2.chiaexplorer.com/balance/' + address)
+    .then((response) => response.json())
     .then((json) => {
       return json;
     })
@@ -27,11 +52,8 @@ export const getBalance = (address) => {
 };
 
 export const getCurrentPrice = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(JSON.parse('{"price":277.53}'));
-    }, 500);
-  })
+  return fetch('https://api2.chiaexplorer.com/currentPrice')
+    .then((response) => response.json())
     .then((json) => {
       return json;
     })
@@ -39,29 +61,3 @@ export const getCurrentPrice = () => {
       console.error(error);
     });
 };
-
-// export const getBalance = (address) => {
-//   return (
-//     fetch('https://api2.chiaexplorer.com/balance/' + address)
-//       // 'https://api2.chiaexplorer.com/balance/xch13v9r3wkceh6h5juecgzcfyf5x6ll9p2ehh03m2z46qyrdutp258qy4zu42'
-//       .then((response) => response.json())
-//       .then((json) => {
-//         // console.log(json);
-//         return json;
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//       })
-//   );
-// };
-
-// export const getCurrentPrice = () => {
-//   return fetch('https://api2.chiaexplorer.com/currentPrice')
-//     .then((response) => response.json())
-//     .then((json) => {
-//       return json;
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// };
